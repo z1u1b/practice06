6712040f489b66b9ba5ab79a0bee6d9749cd1439
z1u1b <ars1for1work@gmail.com>
2025-11-01 16:31:33 +0300

    Complete homework and creating good appeareance for code

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ void destroy(int** mtx, s_t created)
 }
 int** create(s_t rows, s_t cols)
 {
-  size_t created = 0;
+  s_t created = 0;
   int** mtx = new int*[rows];
   try {
     for (; created < rows; ++created) { //(s_t i=0; i<rows; ++i)
@@ -46,28 +46,78 @@ void output(int** mtx, s_t rows, s_t cols)
     std::cout << "\n";
   }
 }
+// homework
+int** convert(const int* t, size_t n, const size_t* lns, size_t rows)
+{
+  // creating and memory allocation
+  s_t created = 0;
+  int** mtx = new int*[rows];
+  try {
+    for (; created < rows; ++created) { //(s_t i=0; i<rows; ++i)
+      mtx[created] = new int[lns[created]];
+    }
+  } catch (const std::bad_alloc& e) {
+    destroy(mtx, created);
+    throw;
+  }
+
+  // initialization
+  s_t countForConvert = 0;
+  for (s_t i = 0; i < rows; ++i) {
+    for (s_t j = 0; j < lns[i]; ++j) {
+      mtx[i][j] = t[countForConvert];
+      countForConvert++;
+    }
+  }
+  return mtx;
+}
+
+void outputForIndefiniteLength(int** arr, s_t* lns, int rows)
+{
+  for (s_t i = 0; i < rows; ++i) {
+    for (s_t j = 0; j < lns[i]; ++j) {
+      std::cout << arr[i][j] << " ";
+    }
+    std::cout << "\n";
+  }
+}
+// end homework
 int main()
 {
-  s_t r = 0, c = 0, init = 0;
-  int** matrix = nullptr;
-  std::cin >> r >> c;
+  // constants
+  int t[] = {5, 5, 5, 5, 6, 6, 7, 7, 7, 7, 7, 8};
+  size_t lns[] = {3, 3, 5, 1};
+  size_t n = 12;
+  size_t rows = 4;
 
-  if (!std::cin) {
-    destroy(matrix, r);
-    std::cerr << "bad input\n";
-    return 2;
-  }
+  // initialization convert array
+  int** convertMatrix = convert(t, n, lns, rows);
+  outputForIndefiniteLength(convertMatrix, lns, rows);
 
+  // clearing memory
+  for (size_t i = 0; i < rows; ++i)
+    delete[] convertMatrix[i];
+  delete[] convertMatrix;
 
-  try {
-    matrix = create(5, 5);
-  } catch (const std::bad_alloc& e) {
-    std::cerr << e.what() << "\n";
-    return 1;
-  }
+  ////classroom
+  // s_t r = 0, c = 0, init = 0;
+  //  std::cin >> r >> c;
 
-  construct(matrix, init, r, c);
-  std::cout << "created!\n";
-  output(matrix, r, c);
-  destroy(matrix, r);
+  // if (!std::cin) {
+  //   destroy(matrix, r);
+  //   std::cerr << "bad input\n";
+  //   return 2;
+  // }
+
+  // try {
+  //   matrix = create(5, 5);
+  // } catch (const std::bad_alloc& e) {
+  //   std::cerr << e.what() << "\n";
+  //   return 1;
+  // }
+  // construct(matrix, init, r, c);
+  // std::cout << "created!\n";
+  // output(matrix, r, c);
+  // destroy(matrix, r);
+  ////end classroom
 }
