1af3ad0161c6dc51f9787ceed9615430d8bdaadd
z1u1b <ars1for1work@gmail.com>
2025-11-01 14:02:15 +0300

    Complete classroom

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@ using s_t = size_t;
 void destroy(int** mtx, s_t created)
 {
   for (s_t i = 0; i < created; i++) {
-    delete [] mtx[i];
+    delete[] mtx[i];
   }
   delete[] mtx;
 }
@@ -11,31 +11,63 @@ int** create(s_t rows, s_t cols)
 {
   size_t created = 0;
   int** mtx = new int*[rows];
-  // for (s_t i=0;i<rows;++i) {
-  //     for (s_t j=0;j<cols;++j) {
-  //         std::cin>>mtx[i][j];
-  //     }
-  // }
   try {
     for (; created < rows; ++created) { //(s_t i=0; i<rows; ++i)
       mtx[created] = new int[cols];
     }
   } catch (const std::bad_alloc& e) {
-    destroy(mtx,created);
+    destroy(mtx, created);
     throw;
-
   }
   return mtx;
 }
+void construct(int** mtx, int init, s_t rows, s_t cols)
+{
+  for (s_t i = 0; i < rows; ++i) {
+    for (s_t j = 0; j < cols; ++j) {
+      mtx[i][j] = init;
+    }
+  }
+}
+void input(int** mtx, s_t rows, s_t cols)
+{
+  for (s_t i = 0; i < rows; ++i) {
+    for (s_t j = 0; j < cols; ++j) {
+      std::cin >> mtx[i][j];
+    }
+  }
+}
+void output(int** mtx, s_t rows, s_t cols)
+{
+  for (s_t i = 0; i < rows; ++i) {
+    for (s_t j = 0; j < cols; ++j) {
+      std::cout << mtx[i][j] << ' ';
+    }
+    std::cout << "\n";
+  }
+}
 int main()
 {
-  // int* r = new int[5];
-
+  s_t r = 0, c = 0, init = 0;
   int** matrix = nullptr;
-  try {
-    matrix=create(5,5);
-  } catch (const std::bad_alloc &e) {
-    std::cerr<<e.what()<<"\n";
+  std::cin >> r >> c;
+
+  if (!std::cin) {
+    destroy(matrix, r);
+    std::cerr << "bad input\n";
+    return 2;
   }
-  destroy(matrix,5);
+
+
+  try {
+    matrix = create(5, 5);
+  } catch (const std::bad_alloc& e) {
+    std::cerr << e.what() << "\n";
+    return 1;
+  }
+
+  construct(matrix, init, r, c);
+  std::cout << "created!\n";
+  output(matrix, r, c);
+  destroy(matrix, r);
 }
